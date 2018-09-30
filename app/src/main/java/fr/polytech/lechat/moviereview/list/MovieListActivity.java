1a5d043d24d440498f36793672ed4bcbf75eb594
Pierre Chat <pierrechat89@hotmail.fr>
2018-09-30 22:02:26 +0200

    TP 4 - Partie 1

--- app/src/main/java/fr/polytech/lechat/moviereview/list/MovieListActivity.java
+++ app/src/main/java/fr/polytech/lechat/moviereview/list/MovieListActivity.java
@@ -1,5 +1,6 @@
 package fr.polytech.lechat.moviereview.list;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -7,6 +8,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.View;
 
 import fr.polytech.lechat.moviereview.R;
+import fr.polytech.lechat.moviereview.categories.CategoriesActivity;
 
 public class MovieListActivity extends AppCompatActivity {
 
@@ -23,6 +25,15 @@ public class MovieListActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 finish();
+            }
+        });
+
+        final MovieListActivity activity = this;
+        findViewById(R.id.list_cat_btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(activity, CategoriesActivity.class);
+                activity.startActivity(intent);
             }
         });
     }
